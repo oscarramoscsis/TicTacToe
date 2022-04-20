7e590c86cf66f56f658318ee5b03ba8e94553d2e
CSIS1400 <9478637@iechs.org>
2022-04-20 10:07:39 -0600

    added fields and some methods to player

--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -1,4 +1,25 @@
 package tictactoe;
 
 public class Player {
+    //fields
+    private String name;
+    private int id;
+
+    //constructor
+
+    public Player(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    //methods
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
