7f7224b51045268597889fe7d34dd949a766976c
nguye <nguyennaruto8362gmail.com>
2022-04-20 22:11:56 -0600

    hi

--- src/GUItest/GUItest.java
+++ src/GUItest/GUItest.java
@@ -1,23 +1,37 @@
 package GUItest;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
-import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+
+
+import javax.swing.ImageIcon;
+//import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 public class GUItest extends JFrame{
-    private JPanel panel1;
-    private JButton button1;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int counter = 0;
+
 private int i;
 private int j;
-
+private JLabel L;
+private BufferedImage marker;
+private GamePanel gamePanel;
 
 
 
@@ -44,35 +58,60 @@ private int j;
          */
         public GUItest() {
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setBounds(100, 100, 500, 500);
+            setBounds(100, 100, 1000, 1000);
             contentPane = new JPanel();
             contentPane.setBackground(Color.WHITE);
             contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
             setContentPane(contentPane);
             contentPane.setLayout(new GridLayout(3, 3, 0, 0));
-//            for (i = 0 ; i< 9; i++) {
+            for (i = 0 ; i< 9; i++) {
             JPanel panel = new JPanel();
             panel.setBorder(new EmptyBorder(5, 5, 5, 5));
             panel.setBackground(Color.RED);
             contentPane.add(panel);
             panel.setLayout(new GridLayout(3, 3, 0, 0));
-//            for (j = 0; j<9; j++) {
-            JPanel panel_1 = new JPanel();
-            panel_1.setBorder(new LineBorder(new Color(255, 200, 0), 3));
-            panel.add(panel_1);
-            panel_1.setLayout(new GridLayout(0, 1, 0, 0));
             
-            JButton button = new JButton("");
-            button.addActionListener(new ActionListener() {
-            	public void actionPerformed(ActionEvent e) {
-            		System.out.print("hi");
+            for (j = 0; j<9; j++) {
+            JPanel panel_1 = new JPanel();
+            panel_1.addMouseListener(new MouseAdapter() {
+            	@Override
+            	public void mouseClicked(MouseEvent e) {
+
+        			JPanel x = (JPanel) e.getComponent();
+            		JLabel y = (JLabel) x.getComponent(0);
+            		counter++;
+            		if (counter % 2 !=0) {
+            			
+            		y.setIcon(new ImageIcon(GUItest.class.getResource("/new/O.png")));
+            		}
+            		else if (counter % 2 == 0)
+            			y.setIcon(new ImageIcon(GUItest.class.getResource("/new/X.png")));
+            		
+            		
+                      
+                    
             	}
             });
-            panel_1.add(button);
-//            }
-//            }
+            panel_1.setBorder(new LineBorder(new Color(255, 200, 0), 3));
+            panel.add(panel_1);
+            panel_1.setLayout(new GridLayout(1, 1, 0, 0));
+            
+            L = new JLabel("");
+            //L.setIcon(new ImageIcon(GUItest.class.getResource("/new/X.png")));
+            panel_1.add(L);
+
+
+            }
+            }
       
 
+        }
+        BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+            BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = resizedImage.createGraphics();
+            graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+            graphics2D.dispose();
+            return resizedImage;
         }
 
         }
--- src/GUItest/GamePanel.java
+++ src/GUItest/GamePanel.java
@@ -1,5 +1,67 @@
 package GUItest;
 
-public class GamePanel {
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+
+public class GamePanel extends JPanel{
+	
+	
+	private ImageIcon icon1;
+	protected JLabel displayImagebody;
+	protected JLabel displayImageroof;
+	protected JLabel displayImagedoor;
+
+	public GamePanel() {
+	setLayout(new GridLayout(0, 1, 0, 0));
+
+	displayImagedoor = new JLabel("");
+	displayImagedoor.setAlignmentX(Component.RIGHT_ALIGNMENT);
+	displayImagedoor.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+	displayImagedoor.setHorizontalAlignment(SwingConstants.CENTER);
+	displayImagedoor.setIcon(new ImageIcon(GUItest.class.getResource("/assets/o.png")));
+	add(displayImagedoor);
+
+	displayImageroof = new JLabel("");
+	displayImageroof.setAlignmentX(Component.RIGHT_ALIGNMENT);
+	displayImageroof.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+	displayImageroof.setHorizontalAlignment(SwingConstants.CENTER);
+	displayImageroof.setIcon(new ImageIcon(GUItest.class.getResource("/assets/x.png")));
+	add(displayImageroof);
+
+	
+	icon1 = new ImageIcon();
+
+	}
+	
+	
+	protected void paintComponet(Graphics g)
+	{
+		super.paintComponent(g);
+		
+		ImageIcon incon1 = new ImageIcon(GamePanel.class.getResource("assets/x.png"));
+		
+	}
+	
+	
+	
+	public void play1()
+	{
+		//icon1 = new ImageIcon(GamePanel.class.getResource("assets/x.png"));
+		
+		JLabel L = new JLabel("");
+		
+		L.setBackground(Color.BLACK);
+		
+	}
+
 
 }
