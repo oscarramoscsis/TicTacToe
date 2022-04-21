08ddcd770b650cc3aed75c78ae54a8252bab24cf
CSIS1400 <9478637@iechs.org>
2022-04-20 21:17:35 -0600

    aksjf;lkasdjf;lk

--- src/tictactoe/majorBoard.java
+++ src/tictactoe/majorBoard.java
@@ -2,30 +2,67 @@ package tictactoe;
 
 public class majorBoard {
 
-    innerBoard board1 = new innerBoard();
-    innerBoard board2 = new innerBoard();
-    innerBoard board3 = new innerBoard();
-    innerBoard board4 = new innerBoard();
-    innerBoard board5 = new innerBoard();
-    innerBoard board6 = new innerBoard();
-    innerBoard board7 = new innerBoard();
-    innerBoard board8 = new innerBoard();
-    innerBoard board9 = new innerBoard();
+    private innerBoard board1 = new innerBoard();
+    private innerBoard board2 = new innerBoard();
+    private innerBoard board3 = new innerBoard();
+    private innerBoard board4 = new innerBoard();
+    private innerBoard board5 = new innerBoard();
+    private innerBoard board6 = new innerBoard();
+    private innerBoard board7 = new innerBoard();
+    private innerBoard board8 = new innerBoard();
+    private innerBoard board9 = new innerBoard();
 
+    private int[][] board = new int[3][3];
 
     innerBoard [][] majorBoardArray = { {board1, board2, board3},
             {board4, board5, board6},
             {board7, board8, board9}};
 
+
     public innerBoard[][] getMajorBoardArray() {
         return majorBoardArray;
     }
 
-    public innerBoard getInnerBoard(int row, int column)
-    {
+    public innerBoard getInnerBoard(int row, int column) {
         return majorBoardArray[row][column];
 
     }
+
+    public boolean checkWin(){
+
+        boolean returnVal = false;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = majorBoardArray[i][j].getWinnerID();
+            }
+        }
+
+        //checks the rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 0)
+                returnVal = true;
+        }
+
+        //checks the columns
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != 0)
+                returnVal = true;
+        }
+
+        //check for diagonals
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != 0 ){
+            returnVal = true;
+        }
+        if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != 0 ){
+            returnVal = true;
+        }
+
+        return returnVal;
+    }
+
+
+
 
     @Override
     public String toString() {
@@ -35,7 +72,9 @@ public class majorBoard {
             for (int j = 0; j < 3; j++) {
                 String tempBoard = majorBoardArray[i][j].toString();
                 returnVal += tempBoard;
-                returnVal += "\n";
+                returnVal += "("+ (i+1) + ", " + (j+1) + ")";
+                returnVal += "\n \n";
+
             }
             returnVal += "\n";
         }
--- src/tictactoe/test.java
+++ src/tictactoe/test.java
@@ -6,71 +6,91 @@ public class test {
 
     public static void main(String[] args) {
 
+        majorBoard mainBoard = new majorBoard();
+        Scanner input = new Scanner(System.in);
+        boolean innerGameIsWon = false;
+        boolean outterGameIsWon = false;
 
+        innerBoard workingBoard = new innerBoard();
+        int counter = 1;
+        int turnId = 1;
 
+        int xCord;
+        int yCord;
 
+        //Creates both players
+        System.out.println("Enter your name! ");
+        String name = input.next();
+        Player player1 = new Player(name, 1);
+        System.out.println("Hello "+name+"! ");
+        System.out.println("Player 2 enter your name! ");
+        name = input.next();
+        Player player2 = new Player(name, 2);
+        System.out.println("Hello "+name+"! ");
 
+        //prints main board
+        System.out.println(mainBoard);
 
+        System.out.println(player1.getName() + ", where would you like to start?");
+        System.out.println("make a move:");
+        xCord = input.nextInt() - 1;
+        yCord = input.nextInt() - 1;
 
+        workingBoard = mainBoard.getInnerBoard(xCord,yCord);
+        System.out.println(workingBoard);
 
+        System.out.println("make a move:");
+        xCord = input.nextInt() - 1;
+        yCord = input.nextInt() - 1;
 
-//        majorBoard myMainBoard = new majorBoard();
-//        innerBoard workingBoard = myMainBoard.getInnerBoard(1, 1);
-//
-//        boolean gameIsWon = false;
-//        boolean moveValid;
-//        int counter = 1;
-//        int turnId;
-//
-//
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Enter your name! ");
-//        String name = input.next();
-//        Player player1 = new Player(name, 1);
-//        System.out.println("Hello "+name+"! ");
-//        System.out.println("PLayer 2 enter your name! ");
-//        name = input.next();
-//        Player player2 = new Player(name, 2);
-//        System.out.println("Hello "+name+"! ");
-//
-//        int xCord;
-//        int yCord;
-//
-//        while(gameIsWon != true) {
-//
-//            if (counter % 2 == 0){
-//                turnId = 2;
-//                System.out.print(player2.getName()+ ", please ");
-//            } else {
-//                turnId = 1;
-//                System.out.print(player1.getName() + ", please ");
-//            }
-//
-//
-//            System.out.println("make a move:");
-//            xCord = input.nextInt() - 1;
-//            yCord = input.nextInt() - 1;
-//
-//            if (workingBoard.checkValid(xCord, yCord)) {
-//                workingBoard.updateBoard(turnId, xCord, yCord);
-//                System.out.println(workingBoard);
-//                gameIsWon = workingBoard.checkWin();
-//            } else
-//                System.out.println("Invalid, try again!");
-//
-//            counter ++;
-//        }
-//
-//        if (counter % 2 == 0){
-//            turnId = 2;
-//            System.out.println(player1.getName() + " wins!");
-//            player1.playerWin();
-//        } else {
-//            turnId = 1;
-//            System.out.println(player2.getName() + " wins!");
-//            player2.playerWin();
-//        }
-//
-//        System.out.println(myMainBoard);
+        workingBoard.updateBoard(turnId, xCord, yCord);
+        System.out.println(workingBoard);
+        innerGameIsWon = workingBoard.checkWin();
+        System.out.println(mainBoard);
+        workingBoard = mainBoard.getInnerBoard(xCord, yCord);
+
+        counter++;
+
+        while(outterGameIsWon != true) {
+            while (innerGameIsWon != true) {
+                System.out.println(workingBoard);
+
+                if (counter % 2 == 0) {
+                    turnId = 2;
+                    System.out.print(player2.getName() + ", please ");
+                } else {
+                    turnId = 1;
+                    System.out.print(player1.getName() + ", please ");
+                }
+
+                System.out.println("make a move:");
+                xCord = input.nextInt() - 1;
+                yCord = input.nextInt() - 1;
+
+                if (workingBoard.checkValid(xCord, yCord)) {
+                    workingBoard.updateBoard(turnId, xCord, yCord);
+                    System.out.println(workingBoard);
+                    innerGameIsWon = workingBoard.checkWin();
+                } else
+                    System.out.println("Invalid, try again!");
+
+                counter++;
+                System.out.println(mainBoard);
+            }
+
+            if (counter % 2 == 0) {
+                turnId = 2;
+                System.out.println(player1.getName() + " wins!");
+                player1.playerWin();
+                workingBoard.setWinnerID(turnId);
+            } else {
+                turnId = 1;
+                System.out.println(player2.getName() + " wins!");
+                player2.playerWin();
+                workingBoard.setWinnerID(turnId);
+            }
+
+            outterGameIsWon = mainBoard.checkWin();
+        }
     }
 }
